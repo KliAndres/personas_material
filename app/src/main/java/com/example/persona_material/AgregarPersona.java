87871bb9287f0d564b0ace8ca28a61b8ef497a49
Carlos Pacheco <cali.pacheco0826@gmail.com>
2020-05-30 18:01:36 -0500

    Conectado a base de datos

--- app/src/main/java/com/example/persona_material/AgregarPersona.java
+++ app/src/main/java/com/example/persona_material/AgregarPersona.java
@@ -34,7 +34,7 @@ public class AgregarPersona extends AppCompatActivity {
         fotos.add(R.drawable.images3);
     }
     public void guardar(View v){
-        String ced, nom, apell;
+        String ced, nom, apell, id;
         int foto;
         Persona persona;
         InputMethodManager imp=(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -42,7 +42,8 @@ public class AgregarPersona extends AppCompatActivity {
         nom= nombre.getText().toString();
         apell= apellido.getText().toString();
         foto = foto_aleatoria();
-        persona=new Persona(ced, nom, apell, foto);
+        id =Datos.getId();
+        persona=new Persona(ced, nom, apell, foto,id);
         persona.guardar();
         limpiar();
         imp.hideSoftInputFromWindow(cedula.getWindowToken(), 0);
