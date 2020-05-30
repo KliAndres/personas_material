96d6fad84a5533fef65e0c6df0c0c94999f760b0
Carlos Pacheco <cali.pacheco0826@gmail.com>
2020-05-30 10:58:40 -0500

    Interfaz detaller persona terminada

--- app/src/main/java/com/example/persona_material/MainActivity.java
+++ app/src/main/java/com/example/persona_material/MainActivity.java
@@ -8,10 +8,14 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -21,8 +25,21 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        RecyclerView lstPersona;
+        FloatingActionButton fab;
+        ArrayList<Persona> personas;
+        LinearLayoutManager llm;
+        Adaptador_Persona adapter;
 
-        FloatingActionButton fab = findViewById(R.id.btnAgregar);
+        lstPersona = findViewById(R.id.lstPersonas);
+        personas = Datos.obtener();
+        llm = new LinearLayoutManager(this);
+        adapter = new Adaptador_Persona(personas);
+        llm.setOrientation(RecyclerView.VERTICAL);
+        lstPersona.setLayoutManager(llm);
+        lstPersona.setAdapter(adapter);
+
+        fab = findViewById(R.id.btnAgregar);
 
 
     }
